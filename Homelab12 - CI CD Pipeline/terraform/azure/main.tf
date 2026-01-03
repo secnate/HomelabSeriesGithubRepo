dbd4231b8c882fee92a1af8150425bd4b7ec8ea8
secnate <33183110+secnate@users.noreply.github.com>
2026-01-03 15:45:07 -0500

    Testing the Azure Jenkinsfile

--- Homelab12 - CI CD Pipeline/terraform/azure/main.tf
+++ Homelab12 - CI CD Pipeline/terraform/azure/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
