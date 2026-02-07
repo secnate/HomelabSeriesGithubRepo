735bf6f854404bec63d256aef96b72416d6e87fb
secnate <33183110+secnate@users.noreply.github.com>
2026-02-07 16:54:55 -0500

    Homelab #16 files

--- Homelab16 - GitOps and Secrets Management/scripts/uninstall_scripts/uninstall_vault.sh
+++ Homelab16 - GitOps and Secrets Management/scripts/uninstall_scripts/uninstall_vault.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# We kill the port forwarding process
-pkill -f "port-forward.*vault" || true
-
 # We start uninstalling the HashiCorp Vault
 helm uninstall vault -n vault
 
