08cc2674d27767bd81f525eb18d7acc854056fa6
secnate <33183110+secnate@users.noreply.github.com>
2026-02-07 17:59:15 -0500

    Homelab #16 files

--- Homelab16 - GitOps and Secrets Management/scripts/setup_scripts/setup_vault.sh
+++ Homelab16 - GitOps and Secrets Management/scripts/setup_scripts/setup_vault.sh
@@ -8,4 +8,8 @@ helm install vault hashicorp/vault -n vault --create-namespace --values ./script
 # Wait for Vault pod ready
 kubectl wait --for=condition=ready pod/vault-0 -n vault --timeout=120s
 
+# This port forwarding was done initially because of issues getting access to the
+# Hashicorp Vault in the computer browser at the https://localhost:8200 IP address
+kubectl port-forward svc/vault-ui 8200:8200 -n vault &
+
 echo "Vault ready -- It Can Be Accessed At http://localhost:8200"
--- Homelab16 - GitOps and Secrets Management/scripts/uninstall_scripts/uninstall_vault.sh
+++ Homelab16 - GitOps and Secrets Management/scripts/uninstall_scripts/uninstall_vault.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Kill any kubectl port-forward processes targeting vault-ui in namespace vault
+# This port forwarding was done initially because of issues getting access to the
+# Hashicorp Vault in the computer browser at the https://localhost:8200 IP address
+pkill -f "kubectl port-forward.*svc/vault-ui.*-n vault" || true
+
 # We start uninstalling the HashiCorp Vault
 helm uninstall vault -n vault
 
