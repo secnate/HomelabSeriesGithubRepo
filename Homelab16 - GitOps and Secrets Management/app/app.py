2823794e6087872a0eee857cac77deb5fcc68bbf
secnate <33183110+secnate@users.noreply.github.com>
2026-02-07 19:32:24 -0500

    Added the Files For Homelab #16 Sample HelloWorld Application

---
+++ Homelab16 - GitOps and Secrets Management/app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    hostname = socket.gethostname()
+    cloud_provider = os.environ.get('CLOUD_PROVIDER', 'Unknown')
+
+    # We now use the render_template() function to serve HTML files
+    #
+    # Per https://flask.palletsprojects.com/en/stable/api/#flask.render_template documentation,
+    # the 'index.html' is the name of the template to render, while the subsequent variables
+    # (cloud, hostname, version) are "context" variables to make available in the template
+    return render_template('index.html', cloud=cloud_provider, hostname=hostname, version='2.0')
+
+@app.route('/health')
+def health():
+    return { 'status' : 'healthy'}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
