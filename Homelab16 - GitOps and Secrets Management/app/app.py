1c64a77287502d5de52dcae463d00ddc8ad1bff7
secnate <33183110+secnate@users.noreply.github.com>
2026-02-14 18:30:03 -0500

    Homelab #16 -- Got the secrets displayed on the sample application

--- Homelab16 - GitOps and Secrets Management/app/app.py
+++ Homelab16 - GitOps and Secrets Management/app/app.py
@@ -9,12 +9,21 @@ def home():
     hostname = socket.gethostname()
     cloud_provider = os.environ.get('CLOUD_PROVIDER', 'Unknown')
 
+    # Extract the secrets that were fed into the Flask application containers environment variables
+    extracted_secrets = {
+        'FEATURE_FLAG_BETA' : os.environ.get('FEATURE_FLAG_BETA'),
+        'API_KEY' : os.environ.get('API_KEY'),
+        'DATABASE_PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
+        'STRIPE_SECRET_KEY' : os.environ.get('STRIPE_SECRET_KEY'),
+        'APP_ENV' : os.environ.get('APP_ENV')
+    }
+
     # We now use the render_template() function to serve HTML files
     #
     # Per https://flask.palletsprojects.com/en/stable/api/#flask.render_template documentation,
     # the 'index.html' is the name of the template to render, while the subsequent variables
     # (cloud, hostname, version) are "context" variables to make available in the template
-    return render_template('index.html', cloud=cloud_provider, hostname=hostname, version='2.0')
+    return render_template('index.html', cloud=cloud_provider, hostname=hostname, version='1.0', **extracted_secrets)
 
 @app.route('/health')
 def health():
